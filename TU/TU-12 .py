d129c57cda6ac1c9a6531f1212b66c8c5fe78790
Sanct1fied <71697782+Sanct1fied@users.noreply.github.com>
2020-09-30 17:23:02 +0800

    Create TU-12 .py

---
+++ TU/TU-12 .py
@@ -0,0 +1,6 @@
+Videogamecharacters = []
+number = int(input("how many characters do you want:"))
+for i in range(number):
+    Character = input("Please add a Video game character:")
+    Videogamecharacters.append(Character)
+print(Videogamecharacters)
