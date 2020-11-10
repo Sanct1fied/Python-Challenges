087bf619f39da3a84c4805cfb01bdfe4418bfbb7
Sanct1fied <71697782+Sanct1fied@users.noreply.github.com>
2020-11-10 18:08:46 +0800

    Create GC21 .py

---
+++ GC/GC21 .py
@@ -0,0 +1,30 @@
+import turtle
+
+for i in range(3):
+    turtle.fd(50)
+    turtle.left(120)
+turtle.penup()
+turtle.fd(100)
+turtle.pendown()
+
+for i in range(4):
+    turtle.fd(50)
+    turtle.left(90)
+    
+turtle.left(180)
+turtle.penup()
+turtle.forward(150)
+turtle.pendown()
+
+for i in range(6):
+    turtle.fd(50)
+    turtle.left(60)
+    
+turtle.right(90)
+turtle.penup()
+turtle.forward(150)
+turtle.pendown()
+
+for i in range(8):
+    turtle.fd(50)
+    turtle.left(45)
