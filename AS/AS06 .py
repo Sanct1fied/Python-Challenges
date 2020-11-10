eadaaa23e20a4bb85b6188803dc8782f2996b9d9
Sanct1fied <71697782+Sanct1fied@users.noreply.github.com>
2020-11-10 17:35:42 +0800

    Create AS06 .py

---
+++ AS/AS06 .py
@@ -0,0 +1,10 @@
+vowel = ["a","e","i","o","u"]
+word = str(input("what is your word?:"))
+integer = 0
+word2 = [char for char in word]
+num = len(word2)
+for i in range(num):
+    if word2[i] in vowel:
+        integer = integer+1
+
+print(integer)
