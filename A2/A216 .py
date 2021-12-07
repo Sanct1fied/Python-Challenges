ae00efef665a5e3a0ed99d114f9e16dce6421608
Sanct1fied <71697782+Sanct1fied@users.noreply.github.com>
2021-12-07 11:37:54 +0800

    Update A216 .py

--- A2/A216 .py
+++ A2/A216 .py
@@ -1,22 +1,41 @@
 import random
+my_words = []
 
 with open("A216.txt","r") as whole_file:
-    wordlist = []
-    word = input("what is your word?")
-    length = len(word)
+    for i in whole_file:
+        i = i.rstrip("\n")
+        my_words.append(i)
+
+
+
+wordlist = []
+word = input("what is your word?")
+length = len(word)
+possible_combinations = []
+
+
+for i in range(length*26):
     characters = [char for char in word]
+    anagram = []
     new_word = []
+    for i in range(length):
+        new_char = random.choice(characters)
+        print(new_char)
+        characters.remove(new_char)
+        new_word += new_char
+        possible_combinations.append(new_word)
+        print(possible_combinations)
 
+        if new_word in possible_combinations:
+            length = length + 1
+            pass
+        elif new_word == characters:
+            length= length + 1
+            pass
+        else:
+            anagram = new_word
 
-    for i in range(length*26):
-        for i in range(length):
-            new_char = random.choice(characters)
-            characters.remove(new_char)
-            new_word.append(new_char)
-            if new_word == characters:
-                length=1
-                pass
-            else:
-                anagram = new_word
-                
-        if anagram in 
+        print(anagram)
+
+        if anagram in my_words:
+            print(anagram)
