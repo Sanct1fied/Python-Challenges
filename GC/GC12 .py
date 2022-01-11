e772fe3d21f1418202b87b62ccc5015eb4e1332c
Sanct1fied <71697782+Sanct1fied@users.noreply.github.com>
2022-01-11 12:22:36 +0800

    Update GC12 .py

--- GC/GC12 .py
+++ GC/GC12 .py
@@ -1,10 +1,26 @@
 import time
+continued = 1
 
-siblings = int(input("how many siblings are there?:"))
-total_time = int(input("How much time do we have (Hours)?:"))
-timeleft = 0
+while continued == 1:
+    siblings = int(input("how many siblings are there?:"))
+    total_time = int(input("How much time do we have (Hours)?:"))
+    total_minutes = total_time*60
+    timeleft = 0
 
-time_per_sibling = total_time/siblings
-print("there is", time_per_sibling, "hours per sibling")
+    time_per_sibling = total_minutes/siblings
+    print("there is", time_per_sibling, "minutes per sibling")
+    siblings_left = siblings
 
-start = input("Start timer?")
+    while siblings_left > 0:
+        start = input("Start timer?")
+        if start in ["yes"]:
+            time_per_sibling = total_minutes/siblings
+            for i in range(int(time_per_sibling)):
+                print(time_per_sibling-i)
+                time.sleep(0.1)
+            print("TIME IS UP")
+            siblings_left -= 1
+
+    print("all the siblings should've had their turn")
+    continued = input("input '0' to stop")
+
