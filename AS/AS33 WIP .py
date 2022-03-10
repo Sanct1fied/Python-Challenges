3c61a93ddab860fb04c4e4e13178eb2b3e7964a8
Sanct1fied <71697782+Sanct1fied@users.noreply.github.com>
2022-03-10 11:47:14 +0800

    Create AS33 WIP .py

---
+++ AS/AS33 WIP .py
@@ -0,0 +1,85 @@
+import random
+
+qs = int(input("how many questions do you want to practice?"))
+hexnumber = ["a","b","c","e","f","1","2","3","4","5","6","7","8","9"]
+binarynumbers = ["0","1"]
+
+def BinaryQ():
+    Binary = []
+    while True:
+
+        for i in range(8):
+            binary = random.choice(binarynumbers)
+            Binary.append(binary)
+        print(*Binary,sep = "")
+
+        answerB = 0
+        for i in range(8,0,-1):
+            answerB = answerB + (int(Binary[i-1])*(2**(8-i)))
+
+        user_answer = int(input("what is your answer?:"))
+        if user_answer == answerB:
+            print("correct")
+            break
+        else:
+            print("incorrect, the answer was" , answerB)
+
+def HexQ():
+    while True:
+
+        Hex = []
+        for i in range(2):
+            hex = random.choice(hexnumber)
+            Hex.append(hex)
+        print(*Hex,sep = "")
+
+
+        answerH = 0
+        answerM = 0
+
+
+        numbers = ["1","2","3","4","5","6","7","8","9"]
+        letters = ["a","b","c","d","e","f"]
+
+
+        if Hex[0] in letters:
+            position = letters.index(Hex[0])
+            answerH = position+10
+            answerH = answerH * 16 ** 1
+        elif Hex[0] in numbers:
+            answerH = int(Hex[0])*16**1
+        if Hex[1] in letters:
+            position = letters.index(Hex[1])
+            answerM = position+10
+            answerM = answerM*16**0
+        elif Hex[1] in numbers:
+            answerM = int(Hex[1])*16**0
+        Hextotal = answerH + answerM
+
+        user_answer = int(input("what is your answer?:"))
+        if user_answer == Hextotal:
+            print("correct")
+            break
+        else:
+            print("incorrect, the answer was" , Hextotal)
+            
+def DenaryQ():
+    Denary = []
+    for i in range(random.randint(2,8)):
+        denary = random.randint(0,9)
+        Denary = Denary.append(denary)
+    print(*Denary, sep="")
+    
+    
+        
+
+
+for i in range(qs):
+    whichQ = input("binary to denary = 1, hex to denary = 2, denary to binary = 3, denary to hex = 4, hex to binary = 5, binary to hex = 6")
+
+    if whichQ in ("2"):
+        HexQ()
+    elif whichQ in ("1"):
+        BinaryQ()
+    elif whichQ in ("3"):
+        DenaryQ()
