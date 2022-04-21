4ba598617a77679084b3fa98848f4682285b530b
Sanct1fied <71697782+Sanct1fied@users.noreply.github.com>
2022-04-21 14:13:49 +0800

    Update AS35 .py

--- AS/AS35 .py
+++ AS/AS35 .py
@@ -1,12 +1,13 @@
 import random
-Affirmative= ["it is certain","it is decidedly so","without a doubt","Yes, definitely","you may rely on it","As I see it, yes","Most likely.","outook good","yes.","signs point to yes"]
+Affirmative= ["it is certain","without a doubt","Yes, definitely","Most likely.","outook good","yes."]
 non_commital = ["cannot predict now","Reply hazy, try again","ask again later","better not tell you now","concentrate and ask again"]
 negative = ["my reply is no","don't count on it","my sources say no","outlook not so good","very doubtful"]
 
 while True:
     Shake = input("what is your question?")
     possible_answers = random.choice([Affirmative,non_commital,negative])
-    answer = random.randint(0,11)
+    print(possible_answers)
+    answer = random.randint(0,5)
 
     print(''.join(possible_answers[answer]))
     play_again = input("do you wish to ask the magic 8 ball another question?")
