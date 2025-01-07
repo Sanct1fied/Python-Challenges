9dc89ae6cd7c95758ee79610d316178522b6b2f3
Sanct1fied <71697782+Sanct1fied@users.noreply.github.com>
2025-01-07 00:07:38 +0000

    Update AS41 .py

--- AS/AS41 .py
+++ AS/AS41 .py
@@ -1,39 +1,52 @@
-choice = input("which parity do you want to practice? odd/even: ")
-answer = ""
 import random
-key = ""
-for i in range(7):
-    temp = str(random.randint(0, 1))
-    key += temp
+import time
+print("welcome to the parity bit checker quiz!")
+time.sleep(1)
 
 
-parity = [char for char in key]
-print(parity)
-one = ["1"]
-zero = ["0"]
-totalone = 0
-totalzero = 0
-num = len(parity)
-for i in range(num):
-    if parity[i] in one:
-        totalone = totalone+1
-    elif parity[i] in zero:
-        totalzero = totalzero+1
+def paritybitchecker(parity):
+    bit = []
+    totalones = 0
 
-if choice == "odd":
-    if totalone != [0, 2, 3, 6, 8]:
-         answer = "1"
-    elif totalone in [0, 2, 3, 6, 8]:
-         answer = "0"
+    answer = ""
+    for i in range(7):
+        byte = random.randint(0,1)
+        bit.append(byte)
+        print(bit)
+    num = len(bit)
+    for i in range(num):
+        if bit[i] == 1:
+            totalones += 1
+        else:
+            pass
+    if parity == "odd":
+        if totalones in [0,2,4,6,8]:
+            answer = "1"
+        if totalones in [1,3,5,7]:
+            answer = "0"
 
-if choice == "even":
-    if totalone != [0, 2, 3, 6, 8]:
-         answer = "1"
-    elif totalone in [0, 2, 3, 6, 8]:
-         answer = "0"
+    if parity== "even":
+        if totalones in [0, 2, 4, 6, 8]:
+            answer = "0"
+        if totalones in [1, 3, 5, 7]:
+            answer = "1"
+    print("the parity is:", parity)
+    useranswer = input("what is your answer?")
+    if useranswer == answer:
+        print("correct")
+    else:
+        print("wrong. the correct answer is:", answer)
 
-useranswer = input("what is the parity digit?: ")
-if useranswer == answer:
-    print("you are right")
-else:
-    print("you are wrong. the answer is", answer)
+true = 1
+while true == 1 :
+    anotherquestion = input("would you like to practice another? Y/N")
+    if anotherquestion in ["Y"]:
+        parity = random.choice(["even", "odd"])
+        paritybitchecker(parity)
+    elif anotherquestion in ["N"]:
+        print("thank you for playing!")
+        time.sleep(2)
+        true = 0
+    else:
+        print("sorry I can only accepy Y/N")
+        pass
